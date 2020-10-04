e5d824aea6e3ade009fe93ad2e08c7f134fa12f9
regandsouza <72354598+regandsouza@users.noreply.github.com>
2020-10-04 22:04:00 +0400

    Update quiz_brain.dart

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -9,27 +9,8 @@ class QuizBrain {
     Question('Second person to land on moon', false,'Michael collins','Buzz aldrin','Michael Buzz','Dwayne Johnson'),
     Question('What planet or moon did curiosity Rover Land', true,'Venus','Moon','Titan','Mars'),
     /*
-    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
-    Question('It is illegal to pee in the Ocean in Portugal.', true),
-    Question(
-        'No piece of square dry paper can be folded in half more than 7 times.',
-        false),
-    Question(
-        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
-        true),
-    Question(
-        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
-        false),
-    Question(
-        'The total surface area of two human lungs is approximately 70 square metres.',
-        true),
-    Question('Google was originally called \"Backrub\".', true),
-    Question(
-        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
-        true),
-    Question(
-        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
-        true),*/
+    ADDITIONAL QUESTIONS TO BE ADDED HERE
+    */
   ];
 
   void nextQuestion() {
